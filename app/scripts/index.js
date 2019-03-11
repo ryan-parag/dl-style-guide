bec1fcf7d5f885ab30cf7b4b232af82fd455964b
Ryan P <parag.ryan@gmail.com>
2019-03-11 16:15:35 -0400

    updated with copy function

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,3 +3,15 @@ import '../styles/main.scss'
 if (process.env.NODE_ENV !== 'production') {
   require('../index.pug')
 }
+
+const swatchButtons = document.querySelectorAll('.swatch__button')
+
+swatchButtons.forEach(button => {
+  button.addEventListener('click', function () {
+    button.querySelector('input').select()
+    document.execCommand('copy')
+    const colorVal = button.querySelector('input').value
+    const colorLabel = button.parentNode.querySelector('.swatch__label').innerHTML
+    alert(' You copied the HEX value for ' + colorLabel + ': ' + colorVal)
+  })
+})
